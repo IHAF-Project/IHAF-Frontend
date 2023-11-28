4c133ecdf3538894ae603071b85070522820c5c8
Ananth <106952901+Ananthm032@users.noreply.github.com>
2023-11-28 14:50:19 +0530

    first commit

--- src/COMPONENTS/NAVBAR/Navbar.jsx
+++ src/COMPONENTS/NAVBAR/Navbar.jsx
@@ -120,10 +120,10 @@ useEffect (() =>{
       </div>
       <div className='navbarsocial-M'>
       <div className="navbar-social">
-      <a href="https://x.com/ihafindia?s=21" ><TwitterIcon sx={{fontSize:'24px',color:'white'}}/></a>
-        <a href="https://instagram.com/ihafindia?igshid=MzRlODBiNWFlZA==" ><InstagramIcon sx={{fontSize:'24px',color:'white'}}/></a>
-        <a href="https://www.facebook.com/profile.php?id=100093003670459&mibextid=LQQJ4d" ><FacebookIcon sx={{fontSize:'24px',color:'white'}}/></a>
-        <a href="https://www.facebook.com/profile.php?id=100093003670459&mibextid=LQQJ4d" ><YouTubeIcon sx={{fontSize:'24px',color:'white'}}/></a>
+      <a href="https://x.com/ihafindia?s=21" target='_blank'><TwitterIcon sx={{fontSize:'24px',color:'white'}}/></a>
+        <a href="https://instagram.com/ihafindia?igshid=MzRlODBiNWFlZA==" target='_blank' ><InstagramIcon sx={{fontSize:'24px',color:'white'}}/></a>
+        <a href="https://www.facebook.com/profile.php?id=100093003670459&mibextid=LQQJ4d" target='_blank' ><FacebookIcon sx={{fontSize:'24px',color:'white'}}/></a>
+        <a href="https://www.facebook.com/profile.php?id=100093003670459&mibextid=LQQJ4d" target='_blank'><YouTubeIcon sx={{fontSize:'24px',color:'white'}}/></a>
       </div>
       <div className='translate-btn-1'>
         <button>
